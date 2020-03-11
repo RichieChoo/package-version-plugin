8d9a58c2d1cbdadf164d29c100fb31aa9f3e1e8a
zhucb <zhucb@bsfit.com.cn>
2020-03-11 18:04:03 +0800

    优化输出文件展示

--- index.js
+++ index.js
@@ -1,55 +1,101 @@
 const fs = require("fs");
+const os = require("os");
 const path = require("path");
 const pluginName = "PackageVersionPlugin";
+const getHtml = require("./lib/getHtml");
+const pkg = require("./package.json");
+const isRegExp = require("lodash.isregexp");
 class PackageVersionPlugin {
 	constructor(options) {
 		const defaultOptions = {
 			showDevDependencies: false,
-			showDependencies: true
+			showDependencies: true,
+			name: pkg.name,
+			outputFile: "version.html",
+			registry: "https://registry.npmjs.org/"
 		};
 		this.options = Object.assign(defaultOptions, options);
+	}
+
+	getPkgVersion(key, context) {
+		const versionPath = path.join(context, "node_modules", key, "package.json");
+		const { version } = JSON.parse(fs.readFileSync(versionPath));
+		return version;
+	}
+
+	getDepAndDevDep({ result, key, keyObj, context, detailPath, regexp }) {
+		if (key === "dependencies") {
+			result.list = [];
+		}
+		Object.keys(keyObj).forEach(v => {
+			let item = {
+				name: v,
+				belong: key,
+				version: this.getPkgVersion(v, context),
+				href: path.join(detailPath, v).replace(path.sep, "/")
+			};
+			if (isRegExp(regexp)) {
+				regexp.lastIndex = 0;
+				if (regexp.test(v)) {
+					result.list.push(item);
+				}
+			} else {
+				result.list.push(item);
+			}
+		});
 	}
 	getData(context) {
 		const packagePath = path.join(context, "package.json");
 		const { dependencies = {}, devDependencies = {} } = JSON.parse(
 			fs.readFileSync(packagePath)
 		);
-		let result = {};
-		const { showDevDependencies, showDependencies } = this.options;
+		const {
+			showDevDependencies,
+			showDependencies,
+			depFilterRegExp,
+			devDepFilterRegExp,
+			registry,
+			...rest
+		} = this.options;
+		let result = {
+			registry: registry.includes("registry.npmjs.org")
+				? "https://www.npmjs.com/"
+				: registry,
+			...rest
+		};
+		const curRegistryDetailPath = registry
+			? registry.includes("registry.npmjs.org")
+				? "https://www.npmjs.com/package/"
+				: path.join(registry, "/#/detail/").replace(path.sep, "/")
+			: false;
 		if (showDependencies) {
-			result.dependencies = dependencies;
-			Object.keys(dependencies).forEach(v => {
-				const versionPath = path.join(
-					context,
-					"node_modules",
-					v,
-					"package.json"
-				);
-				const { version } = JSON.parse(fs.readFileSync(versionPath));
-				result.dependencies[v] = version;
+			this.getDepAndDevDep({
+				result,
+				key: "dependencies",
+				keyObj: dependencies,
+				context,
+				detailPath: curRegistryDetailPath,
+				regexp: depFilterRegExp
 			});
 		}
 		if (showDevDependencies) {
-			result.devDependencies = devDependencies;
-			Object.keys(devDependencies).forEach(v => {
-				const versionPath = path.join(
-					context,
-					"node_modules",
-					v,
-					"package.json"
-				);
-				const { version } = JSON.parse(fs.readFileSync(versionPath));
-				result.devDependencies[v] = version;
+			this.getDepAndDevDep({
+				result,
+				key: "devDependencies",
+				keyObj: devDependencies,
+				context,
+				detailPath: curRegistryDetailPath,
+				regexp: devDepFilterRegExp
 			});
 		}
-
-		return JSON.stringify(result, null, 2);
+		return result;
 	}
 	apply(compiler) {
 		const { hooks, context } = compiler;
 		hooks.emit.tap(pluginName, compilation => {
-			const data = this.getData(context);
-			compilation.assets["version.json"] = {
+			const data = getHtml(this.getData(context));
+			const { outputFile } = this.options;
+			compilation.assets[outputFile] = {
 				source: () => data,
 				size: () => Buffer.byteLength(data, "utf8")
 			};
---
+++ test/test.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const path = require("path");
+const getHtml = require("../lib/getHtml");
+const data = require("./version.json");
+const relativeDate = require("tiny-relative-date");
+const result = {
+	name: "test",
+	updateTime: relativeDate(new Date())
+};
+result.registry = data.registry.includes("registry.npmjs.org")
+	? "https://www.npmjs.com/"
+	: data.registry;
+result.list = Object.keys(data.dependencies).map(v => ({
+	name: v,
+	version: data.dependencies[v],
+	belong: "dependencies",
+	href: result.registry
+		? result.registry.includes("registry.npmjs.org")
+			? "https://www.npmjs.com/package/" + v
+			: path.join(result.registry, "#", "detail", v)
+		: "#"
+}));
+fs.writeFile(
+	path.join(process.cwd(), "test/test-spec.html"),
+	getHtml(result),
+	err => {
+		if (!err) {
+			console.log("test done!");
+		} else {
+			console.log(err);
+		}
+	}
+);
