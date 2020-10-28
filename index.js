833b7260d5fb65ca097dcfe83b32f380d7661f20
zhucb <zhucb@bsfit.com.cn>
2020-10-28 17:33:39 +0800

    rename

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const os = require("os");
 const path = require("path");
-const pluginName = "PackageVersionPlugin";
+const pluginName = "packageVersionPlugin";
 const getHtml = require("./lib/getHtml");
 const getLatest = require("./lib/getLatest");
 const pkg = require("./package.json");
@@ -9,10 +9,9 @@ const isRegExp = require("lodash.isregexp");
 const fecha = require("fecha");
 const internalIp = require("internal-ip");
 const urljoin = require("url-join");
-class PackageVersionPlugin {
+class packageVersionPlugin {
 	constructor(options) {
 		const defaultOptions = {
-			needLastest: false,
 			showDevDependencies: false,
 			showDependencies: true,
 			name: pkg.name,
@@ -21,7 +20,7 @@ class PackageVersionPlugin {
 			ip: internalIp.v4.sync(),
 			buildTime: fecha.format(new Date(), "YYYY-MM-DD HH:mm:ss"),
 			outputFile: "version.html",
-			registry: "https://registry.npmjs.org/"
+			registry: "https://registry.npmjs.org/",
 		};
 		this.options = Object.assign(defaultOptions, options);
 	}
@@ -36,12 +35,12 @@ class PackageVersionPlugin {
 		if (key === "dependencies") {
 			result.list = [];
 		}
-		Object.keys(keyObj).forEach(v => {
+		Object.keys(keyObj).forEach((v) => {
 			let item = {
 				name: v,
 				belong: key,
 				version: this.getPkgVersion(v, context),
-				href: urljoin(detailPath, v)
+				href: urljoin(detailPath, v),
 			};
 			if (isRegExp(regexp)) {
 				regexp.lastIndex = 0;
@@ -70,7 +69,7 @@ class PackageVersionPlugin {
 			registry: registry.includes("registry.npmjs.org")
 				? "https://www.npmjs.com/"
 				: registry,
-			...rest
+			...rest,
 		};
 		const curRegistryDetailPath = registry
 			? registry.includes("registry.npmjs.org")
@@ -84,7 +83,7 @@ class PackageVersionPlugin {
 				keyObj: dependencies,
 				context,
 				detailPath: curRegistryDetailPath,
-				regexp: depFilterRegExp
+				regexp: depFilterRegExp,
 			});
 		}
 		if (showDevDependencies) {
@@ -94,7 +93,7 @@ class PackageVersionPlugin {
 				keyObj: devDependencies,
 				context,
 				detailPath: curRegistryDetailPath,
-				regexp: devDepFilterRegExp
+				regexp: devDepFilterRegExp,
 			});
 		}
 		return result;
@@ -102,26 +101,22 @@ class PackageVersionPlugin {
 	apply(compiler) {
 		const { hooks, context } = compiler;
 		hooks.emit.tapAsync(pluginName, (compilation, cb) => {
-			const { outputFile, needLastest } = this.options;
+			const { outputFile } = this.options;
 			const result = this.getData(context);
-			const callback = result => {
+			const callback = (result) => {
 				const data = getHtml(result);
 				compilation.assets[outputFile] = {
 					source: () => data,
-					size: () => Buffer.byteLength(data, "utf8")
+					size: () => Buffer.byteLength(data, "utf8"),
 				};
 				cb();
 			};
-			if (needLastest) {
-				Promise.all(result.list.map(v => getLatest(v))).then(res => {
-					result.list = res;
-					callback(result);
-				});
-			} else {
+			Promise.all(result.list.map((v) => getLatest(v))).then((res) => {
+				result.list = res;
 				callback(result);
-			}
+			});
 		});
 	}
 }
 
-module.exports = PackageVersionPlugin;
+module.exports = packageVersionPlugin;
